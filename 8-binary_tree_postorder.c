89be396b09dfdc1a89d49ccbe5511bd57771ae87
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-01-31 10:23:39 +0100

    corrections

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,6 +5,7 @@
  * @tree: A pointer to the root node.
  * @func: A pointer to a function to call
  */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
